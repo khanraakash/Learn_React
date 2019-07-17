cdd75b2cbdde88124c9daf7bf192062bc5d989c3
khanraakash <khanraakash@gmail.com>
2019-07-17 07:15:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className="grey-bg">
-        <img src="https://i.dailymail.co.uk/i/pix/2015/09/01/18/2BE1E88B00000578-3218613-image-m-5_1441127035222.jpg" alt="" className="image"/>
-        <p style={{ backgroundColor:"grey",fontSize:"16px"}}>
-          <hr/>
-          <br/>
-          Hi it's a image  from Google
-        </p>
+
         </div>
     </div>
   );
